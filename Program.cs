7df5987a9b9c6922261912786a4a4a49af3adfd6
MotieeAtefe <motiee1377@gmaile.com>
2024-10-19 19:08:58 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new { Name = "John Doe", Age = 25, Adress = "123 Main street" };
+            File.WriteAllText();
+        }
+    }
+}
